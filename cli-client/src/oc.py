839d2d6e34c3b321d3e2be00c66b0b3999366c57
Ben Christel <bchristel@pivotal.io>
2019-07-02 21:29:34 -0700

    Auth with username/password if provided

--- cli-client/src/oc.py
+++ cli-client/src/oc.py
@@ -1,10 +1,14 @@
 import subprocess
 import json
+import ocrc
 
 def cabbages():
-    return Client().cabbages()
+    return Client(ocrc_path="~/.ocrc").cabbages()
 
 class Client():
+    def __init__(self, ocrc_path):
+        self.ocrc_path = ocrc_path
+
     def cabbages(self):
         """
         I have it on good authority that this returns
@@ -21,13 +25,21 @@ class Client():
         But really, it just returns whatever JSON the API
         returns, parsed into a Python dict.
         """
-        return parse_json(stdout_of(cabbages_command()))
+        return parse_json(stdout_of(self.cabbages_command()))
+
+    def cabbages_command(self):
+        return ["oc", "cabbages"] + self.auth_args()
+
+    def auth_args(self):
+        try:
+            with open(self.ocrc_path, 'r') as f:
+                username, password = ocrc.get_credentials_exported_by(f.read())
+            return ["--username", username, "--password", password]
+        except IOError:
+            return []
 
 def parse_json(str):
     return json.loads(str)
 
 def stdout_of(cmd):
     return subprocess.check_output(cmd)
-
-def cabbages_command():
-    return ["oc", "cabbages"]
--- cli-client/src/oc_test.py
+++ cli-client/src/oc_test.py
@@ -8,8 +8,13 @@ def test_json():
 def test_calling_a_command_and_getting_stdout():
     assert oc.stdout_of(['echo', 'hi']) == 'hi\n'
 
-def test_integration_through_class():
-    assert oc.Client().cabbages() == dict()
+def test_integration():
+    assert (oc.Client(ocrc_path="fakes/ocrc").cabbages()
+        == dict(args=['cabbages', '--username', 'elias', '--password', '`a$1""!`']))
 
 def test_integration_through_global_function():
-    assert oc.cabbages() == dict()
+    assert oc.cabbages()['args'][0] == 'cabbages'
+
+def test_integration_when_no_credentials():
+    assert (oc.Client(ocrc_path="i-do-not-exist").cabbages()
+        == dict(args=['cabbages']))
