a6128fc7795d00deb060180aed083e12e703c057
Ben Christel <bchristel@pivotal.io>
2019-07-02 19:36:47 -0700

    Make the oc interface easier for clients to mock
    and add some integration tests, since I got the python class syntax wrong on
    the first try and my tests didn't catch it :(

--- cli-client/src/oc_test.py
+++ cli-client/src/oc_test.py
@@ -1,12 +1,15 @@
 import oc
 import types
 
-def test_cabbages_exists():
-    assert type(oc.cabbages) == types.FunctionType
-
 def test_json():
     assert oc.parse_json('{"foo": "bar"}') == dict(foo='bar')
     assert oc.parse_json('["hello", "world"]') == ['hello', 'world']
 
 def test_calling_a_command_and_getting_stdout():
     assert oc.stdout_of(['echo', 'hi']) == 'hi\n'
+
+def test_integration_through_class():
+    assert oc.Client().cabbages() == dict()
+
+def test_integration_through_global_function():
+    assert oc.cabbages() == dict()
