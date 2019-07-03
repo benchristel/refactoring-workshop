fa7297372b4d5b74d00f8f4a649e35470fdee5fc
Ben Christel <bchristel@pivotal.io>
2019-07-02 22:09:07 -0700

    Add the option of passing credentials as arguments

--- cli-client/src/oc.py
+++ cli-client/src/oc.py
@@ -2,11 +2,13 @@ import subprocess
 import json
 import ocrc
 
-def cabbages():
-    return Client(ocrc_path="~/.ocrc").cabbages()
+def cabbages(username=None, password=None):
+    return Client(ocrc_path="~/.ocrc", username=username, password=password).cabbages()
 
 class Client():
-    def __init__(self, ocrc_path):
+    def __init__(self, ocrc_path='', username=None, password=None):
+        self.username = username
+        self.password = password
         self.ocrc_path = ocrc_path
 
     def cabbages(self):
@@ -31,22 +33,34 @@ class Client():
         return ["oc", "cabbages"] + self.creds().oc_args()
 
     def creds(self):
-        try:
-            with open(self.ocrc_path, 'r') as f:
-                return Credentials(f.read())
-        except IOError:
-            return NullCredentials()
+        if self.username == None or self.password == None:
+            try:
+                with open(self.ocrc_path, 'r') as f:
+                    return CredentialsFromFile(f.read())
+            except IOError:
+                return NullCredentials()
+        else:
+            return CredentialsFromArgs(self.username, self.password)
 
 class NullCredentials:
     def oc_args(self):
         return []
 
-class Credentials:
-    def __init__(self, script):
-        self.username, self.password = ocrc.get_credentials_exported_by(script)
+class CredentialsFromArgs:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
     def oc_args(self):
         return ['--username', self.username, '--password', self.password]
+
+class CredentialsFromFile:
+    def __init__(self, script):
+        username, password = ocrc.get_credentials_exported_by(script)
+        self.delegate = CredentialsFromArgs(username, password)
+
+    def oc_args(self):
+        return self.delegate.oc_args()
 
 def parse_json(str):
     return json.loads(str)
--- cli-client/src/oc_test.py
+++ cli-client/src/oc_test.py
@@ -8,6 +8,17 @@ def test_json():
 def test_calling_a_command_and_getting_stdout():
     assert oc.stdout_of(['echo', 'hi']) == 'hi\n'
 
+def test_credentials_passed_as_args():
+    assert (oc.Client(username="bob", password="foo").creds().oc_args()
+        == ['--username', 'bob', '--password', 'foo'])
+
+def test_credentials_parsed_from_file():
+    assert (oc.Client(ocrc_path="fakes/ocrc").creds().oc_args()
+        == ['--username', 'elias', '--password', '`a$1""!`'])
+
+def test_no_credentials():
+    assert oc.Client().creds().oc_args() == []
+
 def test_integration():
     assert (oc.Client(ocrc_path="fakes/ocrc").cabbages()
         == dict(args=['cabbages', '--username', 'elias', '--password', '`a$1""!`']))
@@ -18,3 +29,7 @@ def test_integration_through_global_function():
 def test_integration_when_no_credentials():
     assert (oc.Client(ocrc_path="i-do-not-exist").cabbages()
         == dict(args=['cabbages']))
+
+def test_integration_when_credentials_passed_as_args():
+    assert (oc.Client(username="a", password="b").cabbages()
+        == dict(args=['cabbages', '--username', 'a', '--password', 'b']))
