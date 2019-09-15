886c7c82ccad495412505302631c7f390d9c30e6
Ben Christel <bchristel@pivotal.io>
2019-09-15 09:22:24 -0700

    Consolidate workshop code at the top level
    I've had success running a refactoring workshop with autoclop.rb, so I'm
    consolidating this repository to focus only on that kata.

--- cli-client/python/src/oc_test.py
+++
@@ -1,4 +0,0 @@
-import oc
-
-def test_cabbages():
-    assert oc.cabbages() == "fixme"
--- cli-client/python/test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-DIR_OF_THIS_SCRIPT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-pushd "$DIR_OF_THIS_SCRIPT" >/dev/null
-  bin/pytest
-popd >/dev/null
