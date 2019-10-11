e79b0b3e9114e03e121c5b0a060e225466c7da22
Ben Christel <bchristel@pivotal.io>
2019-10-11 08:44:06 -0700

    Decouple tests from the autoclop() method
    The global variable smell can't be removed unless participants can refactor the
    interface between run_autoclop() and autoclop(). I want to enable them to do
    that refactoring without changing tests, because changing tests is out of scope
    for this workshop.

--- spec/autoclop_spec.rb
+++ spec/autoclop_spec.rb
@@ -16,13 +16,34 @@ describe 'Configuring `clop`' do
     allow(File).to receive(:read).with('/etc/issue').and_return('linux')
   end
 
-  context 'if you do not provide a config file path' do
-    before(:each) { ENV['AUTOCLOP_CONFIG'] = nil }
+  def config=(yaml)
+    ENV['AUTOCLOP_CONFIG'] = '/tmp/test-file-deleteme'
+    File.write('/tmp/test-file-deleteme', yaml)
+  end
 
+  shared_examples_for 'the defaults' do
     it 'uses the defaults' do
       run_autoclop
       expect(Kernel).to have_received(:system).with("clop configure --python 2 -O2 -L/home/#{user}/.cbiscuit/lib")
     end
+
+    context 'on Red Hat 8' do
+      before :each do
+        allow(File).to receive(:read).with('/etc/issue')
+          .and_return('blah blah Red Hat 8 blah')
+      end
+
+      it 'uses python 3' do
+        run_autoclop
+        expect(Kernel).to have_received(:system).with("clop configure --python 3 -O2 -L/home/#{user}/.cbiscuit/lib")
+      end
+    end
+  end
+
+  context 'if you do not provide a config file path' do
+    before(:each) { ENV['AUTOCLOP_CONFIG'] = nil }
+
+    it_behaves_like 'the defaults'
 
     it 'warns' do
       run_autoclop
@@ -30,17 +51,25 @@ describe 'Configuring `clop`' do
     end
   end
 
-  context 'if you provide a file that is not YAML' do
+  context 'given an empty config file path' do
+    before(:each) { ENV['AUTOCLOP_CONFIG'] = '' }
+
+    it_behaves_like 'the defaults'
+
+    it 'warns' do
+      run_autoclop
+      expect(Kernel).to have_received(:puts).with('WARNING: No file specified in $AUTOCLOP_CONFIG. Assuming the default configuration.')
+    end
+  end
+
+  context 'given a file that is not YAML' do
     before :each do
       file = '/tmp/test-file-deleteme'
       FileUtils.touch file
       ENV['AUTOCLOP_CONFIG'] = file
     end
 
-    it 'uses the defaults' do
-      run_autoclop
-      expect(Kernel).to have_received(:system).with("clop configure --python 2 -O2 -L/home/#{user}/.cbiscuit/lib")
-    end
+    it_behaves_like 'the defaults'
 
     it 'warns' do
       run_autoclop
@@ -48,121 +77,118 @@ describe 'Configuring `clop`' do
     end
   end
 
-  it 'uses python 3 on Red Hat 8' do
-    $os = 'Linux version 2.6.32-71.el6.x86_64 (mockbuild@c6b6.centos.org) (gcc version 4.4.4 20100726 (Red Hat 8.0.0) (GCC) ) #1 SMP Fri May 20 03:51:51 BST 2011  '
-    $config = ''
-    autoclop
-    expect(Kernel).to have_received(:system).with("clop configure --python 3 -O2 -L/home/#{user}/.cbiscuit/lib")
+  context 'on Red Hat 8' do
+    before :each do
+      allow(File).to receive(:read).with('/etc/issue')
+        .and_return('Linux version 2.6.32-71.el6.x86_64 (mockbuild@c6b6.centos.org) (gcc version 4.4.4 20100726 (Red Hat 8.0.0) (GCC) ) #1 SMP Fri May 20 03:51:51 BST 2011  ')
+      self.config = "---\n{}"
+    end
+
+    it 'uses python 3' do
+      run_autoclop
+      expect(Kernel).to have_received(:system).with("clop configure --python 3 -O2 -L/home/#{user}/.cbiscuit/lib")
+    end
   end
 
   it 'uses the python version specified in the config file, if any' do
-    $config = '/tmp/test-file-deleteme'
-    File.write($config, <<-EOF)
+    self.config = <<-EOF
 ---
 python-version: 2.7
 EOF
-    autoclop
+    run_autoclop
     expect(Kernel).to have_received(:system).with("clop configure --python 2.7 -O2 -L/home/#{user}/.cbiscuit/lib")
   end
 
   it 'uses the optimization level specified in the config file, if any' do
-    $config = '/tmp/test-file-deleteme'
-    File.write($config, <<-EOF)
+    self.config = <<-EOF
 ---
 opt: O0
 EOF
-    autoclop
+    run_autoclop
     expect(Kernel).to have_received(:system).with("clop configure --python 2 -O0 -L/home/#{user}/.cbiscuit/lib")
   end
 
   it 'gets libs from a specified directory' do
-    $config = '/tmp/test-file-deleteme'
-    File.write($config, <<-EOF)
+    self.config = <<-EOF
 ---
 libdir: /foo/bar
 EOF
-    autoclop
+    run_autoclop
     expect(Kernel).to have_received(:system).with('clop configure --python 2 -O2 -L/foo/bar')
   end
 
   it 'gets libs from multiple directories' do
-    $config = '/tmp/test-file-deleteme'
-    File.write($config, <<-EOF)
+    self.config = <<-EOF
 ---
 libdirs:
 - /foo/bar
 - /baz/quux
 EOF
-    autoclop
+    run_autoclop
     expect(Kernel).to have_received(:system).with('clop configure --python 2 -O2 -L/foo/bar -L/baz/quux')
   end
 
   it 'loads specific libraries' do
-    $config = '/tmp/test-file-deleteme'
-    File.write($config, <<-EOF)
+    self.config = <<-EOF
 ---
 libs:
 - /foo/liba.1.0
 - /foo/libb.2.0
 EOF
-    autoclop
+    run_autoclop
     expect(Kernel).to have_received(:system).with('clop configure --python 2 -O2 -l/foo/liba.1.0 -l/foo/libb.2.0')
   end
 
   it 'handles libraries with spaces in their names' do
-    $config = '/tmp/test-file-deleteme'
-    File.write($config, <<-EOF)
+    self.config = <<-EOF
 ---
 libs:
 - /foo/weird lib.1.0
 EOF
-    autoclop
+    run_autoclop
     expect(Kernel).to have_received(:system).with('clop configure --python 2 -O2 -l/foo/weird\ lib.1.0')
   end
 
   it 'blocks shell injection attacks that exploit the optimization level' do
-    $config = '/tmp/test-file-deleteme'
-    File.write($config, <<-EOF)
+    self.config = <<-EOF
 ---
 opt: '$(echo hacked > /tmp/foo)'
 libs: []
 EOF
-    autoclop
+    run_autoclop
     expect(Kernel).to have_received(:system).with('clop configure --python 2 -\$\(echo\ hacked\ \>\ /tmp/foo\)')
   end
 
   it 'blocks shell injection attacks that exploit the python version' do
-    $config = '/tmp/test-file-deleteme'
-    File.write($config, <<-EOF)
+    self.config = <<-EOF
 ---
 python-version: '$(echo hacked > /tmp/foo)'
 libs: []
 EOF
-    autoclop
+    run_autoclop
     expect(Kernel).to have_received(:system).with('clop configure --python \$\(echo\ hacked\ \>\ /tmp/foo\) -O2')
   end
 
   it 'blocks shell injection attacks that exploit the USER env variable' do
     ENV['USER'] = '$(echo hacked > /tmp/foo)'
-    autoclop
+    self.config = ''
+    run_autoclop
     expect(Kernel).to have_received(:system).with(include('\$\(echo\ hacked\ \>\ /tmp/foo\)'))
   end
 
   it 'blocks shell injection attacks that exploit the USER env variable, when a config file is given' do
-    $config = '/tmp/test-file-deleteme'
-    File.write($config, <<-EOF)
+    self.config = <<-EOF
 ---
 python-version: 2
 EOF
     ENV['USER'] = '$(echo hacked > /tmp/foo)'
-    autoclop
+    run_autoclop
     expect(Kernel).to have_received(:system).with(include('\$\(echo\ hacked\ \>\ /tmp/foo\)'))
   end
 
   it 'raises an error if clop fails' do
-    $config = '/tmp/test-file-deleteme'
-    File.write($config, '{}')
+    self.config = '{}'
     allow(Kernel).to receive(:system).and_return(false)
-    expect { autoclop }.to raise_error 'clop failed. Please inspect the output above to determine what went wrong.'
+    expect { run_autoclop }.to raise_error 'clop failed. Please inspect the output above to determine what went wrong.'
   end
 end
